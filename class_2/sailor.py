e1503cb9d12fecc76cdc7d8af8e17d950d3300b2
eyakub <eyakubsorkar@gmail.com>
2025-03-16 05:59:38 +0600

    Task of class 2

--- class_2/sailor.py
+++ class_2/sailor.py
@@ -11,13 +11,11 @@ driver.get("https://www.sailor.clothing/category/shirt-gauir")
 products = {}
 
 for product_no in range(1, 20):
-    text = f'//*[@id="__next"]/main/section[2]/div[2]/div[2]/div[2]/div/div[{product_no}]'
-    product = driver.find_element(By.XPATH, text)
+    PRODUCT_XPATH = f'//*[@id="__next"]/main/section[2]/div[2]/div[2]/div[2]/div/div[{product_no}]'
+    product = driver.find_element(By.XPATH, PRODUCT_XPATH)
     link = product.find_element(By.TAG_NAME, "a")
     img = product.find_element(By.TAG_NAME, "img")
-    title = product.find_element(
-        By.XPATH,
-        f'//*[@id="__next"]/main/section[2]/div[2]/div[2]/div[2]/div/div[{product_no}]/div[2]/h4')
+    title = product.find_element(By.TAG_NAME, "h4")
 
     products[product_no] = {
         "title": title.text,
