e9af25f2c9b153b822d85137331ed2314c40bf75
eyakub <eyakubsorkar@gmail.com>
2025-03-16 05:51:50 +0600

    Task of class 2

---
+++ sailor.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Edge()
+driver.maximize_window()
+driver.set_page_load_timeout(300)
+
+driver.get("https://www.sailor.clothing/category/shirt-gauir")
+
+products = {}
+
+for product_no in range(1, 20):
+    text = f'//*[@id="__next"]/main/section[2]/div[2]/div[2]/div[2]/div/div[{product_no}]'
+    product = driver.find_element(By.XPATH, text)
+    link = product.find_element(By.TAG_NAME, "a")
+    img = product.find_element(By.TAG_NAME, "img")
+    title = product.find_element(
+        By.XPATH,
+        f'//*[@id="__next"]/main/section[2]/div[2]/div[2]/div[2]/div/div[{product_no}]/div[2]/h4')
+
+    products[product_no] = {
+        "title": title.text,
+        "link": link.get_attribute("href"),
+        "img": img.get_attribute("src")
+    }
+
+print(products)
+
+driver.quit()
